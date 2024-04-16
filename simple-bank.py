0a05321876a2ae85062737022f5905bc911259fc
Andrei Lemisz <117089381+andreilemisz@users.noreply.github.com>
2024-04-15 21:52:04 -0300

    Add files via upload

--- simple-bank.py
+++ simple-bank.py
@@ -2,6 +2,7 @@
 from abc import ABC, abstractmethod
 import random
 import os
+import json
 
 ############ Banks and Agencies
 """
@@ -21,10 +22,10 @@ Numbers 6, 7, 8
 Agencies 9, 10
 Numbers 9, 10
 """
-american_bank = ["1", "2", "3"]
-french_bank = ["4", "5"]
-swiss_bank = ["6", "7", "8"]
-brazilian_bank = ["9", "10"]
+american_bank = [1, 2, 3]
+french_bank = [4, 5]
+swiss_bank = [6, 7, 8]
+brazilian_bank = [9, 10]
 
 ############
 
@@ -44,6 +45,7 @@ class Person(ABC):
     
     def set_age(self, age):
         self._age = age
+        
     
     @abstractmethod
     def __repr__(self) -> str:
@@ -72,99 +74,106 @@ class Client(Person):
                     print("You did not choose a type of account. Try again.")
 
     def __repr__(self) -> str:
-        if self.account:
-            return f"Person is {self._name}\nAge {self._age}\n\
-Account type {self.account.type}\nBank {self.account.bank}\n\
-Agency {self.account.agency}\nNumber {self.account.number}"
-        
-        return f"Person is {self._name} | Age {self._age}"
+        return f"{self.type}"
 
 class Account(ABC):
-    def __init__(self) -> None:
+    def __init__(self):
         pass
     
     def open_account(self):
         while True:
-            print("Which bank would you like to choose?")
+            print("\nWe work with 4 differente banks, which one would you like to be yours?")
             self.bank = input("[1] American\n[2] French\n[3] Swiss\n[4] Brazilian\n")
             match self.bank:
                 case "1":
                     self.bank = "American"
-                    self.agency = random.randrange(1,3)
-                    self.number = random.randrange(1,3)
+                    self.agency = random.randrange((american_bank[0]),american_bank[-1])
+                    self.number = random.randrange((american_bank[0]),american_bank[-1])
                     break
                 case "2":
                     self.bank = "French"
-                    self.agency = random.randrange(4,5)
-                    self.number = random.randrange(4,5)
+                    self.agency = random.randrange((french_bank[0]),french_bank[-1])
+                    self.number = random.randrange((french_bank[0]),french_bank[-1])
                     break
                 case "3":
                     self.bank = "Swiss"
-                    self.agency = random.randrange(6,8)
-                    self.number = random.randrange(6,8)
+                    self.agency = random.randrange((swiss_bank[0]),swiss_bank[-1])
+                    self.number = random.randrange((swiss_bank[0]),swiss_bank[-1])
                     break
                 case "4":
                     self.bank = "Brazilian"
-                    self.agency = random.randrange(9,10)
-                    self.number = random.randrange(9,10)
+                    self.agency = random.randrange((brazilian_bank[0]),brazilian_bank[-1])
+                    self.number = random.randrange((brazilian_bank[0]),brazilian_bank[-1])
                     break
                 case _:
                     print("You did not choose a valid bank. Try again.")
-        print(f"You chose the {self.bank} bank.")
-        print(f"Your agency number is {self.agency}")
-        print(f"Your account number is {self.number}")
-        
+        os.system("cls")
+        print(f"The bank issue an Agency number and an Account number. \
+You should save these numbers to use in our authentication process later.\n\n \
+    Your bank is {self.bank} bank.\n \
+    Your agency number is {self.agency}.\n \
+    Your account number is {self.number}.\n \
+    Your account is a {self}")
+
+    @abstractmethod
     def deposit_money(self):
         while True:
             self.amount = input("How much would you like to deposit?\n")
-            self.value = 0
             try:
                 self.amount = float(self.amount)
-                self.value += self.amount
-                print(f"Your new amount of money is {self.value}.")
+                self.money_in_bank += self.amount
+                print(f"Your new amount of money is ${self.money_in_bank}.")
                 break
             except:
                 print("Please, only type valid numbers. Try again.")
     
-    # @abstractmethod
+    @abstractmethod
     def withdrawn_money(self):
         while True:
             self.amount = input("How much would you like to withdrawn?\n")
             try:
                 self.amount = float(self.amount)
                 self.value -= self.amount
-                print(f"Your new amount of money is {self.value}.")
+                print(f"Your new amount of money is ${self.value}.")
                 break
             except:
                 print("Please, only type valid numbers. Try again.")
     
-    # @abstractmethod
+    @abstractmethod
     def __repr__(self) -> str:
-        return f"Account type {self.type}\nBank {self.bank}\n\
-Agency {self.agency}\nNumber {self.number} "
+        return f"{self.type}"
 
 class CheckingsAccount(Account):
     def __init__(self):
         super().__init__()
         self.type="Checkings Account"
-        
+
+    def deposit_money(self):
+        while True:
+            self.amount = input("How much would you like to deposit?\n")
+            try:
+                self.amount = float(self.amount)
+                self.money_in_bank += self.amount
+                print(f"Your new amount of money is ${self.money_in_bank}.")
+                break
+            except:
+                print("Please, only type valid numbers. Try again.")
         
     def withdrawn_money(self):
         while True:
             self.amount = input("How much would you like to withdrawn?\n")
-            self.value = self.client_info.value
             try:
                 self.amount = float(self.amount)
-                self.result = self.value - self.amount
+                self.result = self.money_in_bank - self.amount
                 if self.result >= 0:
-                    self.value -= self.amount
-                    print(f"You withdrew {self.amount}. You now have {self.value} \
+                    self.money_in_bank -= self.amount
+                    print(f"You withdrew ${self.amount}. You now have ${self.money_in_bank} \
 in your account.")
                     break
                 elif self.result < 0 and self.result >= float(-500):
-                    self.value -= self.amount
+                    self.money_in_bank -= self.amount
                     print(f"Your withdrawn left you owning the bank, but we still \
-lend you some. Your new amount in our account is {self.value}.")
+lend you some. Your new amount in our account is ${self.money_in_bank}.")
                     break
                 else:
                     print("We cannot allow you to withdrawn that amount of money, \
@@ -172,40 +181,50 @@ or you will be more in debt than we can allow.")
                     break
             except:
                 print("Please, only type valid numbers. Try again.")
-        
+                
+
     def __repr__(self) -> str:
-        return f"Your Checkings Account Details:\nBank {self.bank}\n\
-Agency {self.agency}\nNumber {self.number}"
+        return f"{self.type}"
 
 class SavingsAccount(Account):
     def __init__(self):
         super().__init__()
         self.type="Savings Account"
         
+    def deposit_money(self):
+        while True:
+            self.amount = input("How much would you like to deposit?\n")
+            try:
+                self.amount = float(self.amount)
+                self.money_in_bank += self.amount
+                print(f"Your new amount of money is ${self.money_in_bank}.")
+                break
+            except:
+                print("Please, only type valid numbers. Try again.")
+        
     def withdrawn_money(self):
         while True:
             self.amount = input("How much would you like to withdrawn?\n")
-            self.value = self.client_info.value
             try:
                 self.amount = float(self.amount)
-                if self.value - self.amount < 0:
+                if self.money_in_bank - self.amount < 0:
                     print(f"We cannot allow you to withdrawn that amount of money \
-from your savings account. You only have {self.value}.")
+from your savings account. You only have ${self.money_in_bank}.")
                     break
                 else:
-                    self.value -= self.amount
-                    print(f"You withdrawn {self.amount} from your savings account.\
-You now have {self.value}.")
+                    self.money_in_bank -= self.amount
+                    print(f"You withdrawn ${self.amount} from your savings account.\
+You now have ${self.money_in_bank}.")
                     break
             except:
                 print("Please, only type valid numbers. Try again.")
                 
     def __repr__(self) -> str:
-        return f"Your Savings Account Details:\nBank {self.bank}\n\
-Agency {self.agency}\nNumber {self.number}"
+        return f"{self.type}"
 
 class Bank():
     def __init__(self) -> None:
+        self.money_in_bank = 0
         pass
     
     def client_information(self):
@@ -213,16 +232,23 @@ class Bank():
         age = input("What is your age?\n")
         self.client_info = Client(username, age)
         self.type_account = Client.open_account(self)
-        self.account_info = Account()
-        Account.open_account(self.account_info)
+        if self.type_of_account == "checkings":
+                self.account_info = CheckingsAccount()
+        elif self.type_of_account == "savings":
+                self.account_info = SavingsAccount()
+        self.account_info.open_account()
         
     def deposit_money_bank(self):
         print("You chose to deposit. We need to verify your information first.")
         Bank.authentication(self)
         if self.authenticated == True:
-            self.account_value = Account.deposit_money(self.client_info)
-            self.authenticated = None
-        
+            if self.type_of_account == "checkings":
+                CheckingsAccount.deposit_money(self)
+                self.authenticated = None
+            elif self.type_of_account == "savings":
+                SavingsAccount.deposit_money(self)
+                self.authenticated = None
+            
     def authentication(self):
         while True:
             self.bank_name_auth = input("What is your bank?\n[1] American\n[2] French\n[3] Swiss\n[4] Brazilian\n")
@@ -240,9 +266,6 @@ class Bank():
             self.bank_agency_auth = input("What is your agency number?\n")
             self.bank_account_auth = input("What is your account number?\n")
             os.system("cls")
-            print(f"Banco escolhido {self.bank_name_auth} - Banco Determinado {self.account_info.bank}\n \
-                Agencia escolhida {self.bank_agency_auth} - Agencia Determinada {self.account_info.agency}\n \
-                    Conta escolhida {self.bank_account_auth} - Conta Determinada {self.account_info.number}")
             if self.bank_name_auth == self.account_info.bank \
                 and int(self.bank_agency_auth) == self.account_info.agency \
                 and int(self.bank_account_auth) == self.account_info.number:
@@ -256,50 +279,53 @@ class Bank():
         print("You chose to withdrawn. We need to verify your information first.")
         Bank.authentication(self)
         if self.authenticated == True:
-            if self.client_info.type_of_account == "checkings":
+            if self.type_of_account == "checkings":
                 CheckingsAccount.withdrawn_money(self)
                 self.authenticated = None
-            elif self.client_info.type_of_account == "savings":
+            elif self.type_of_account == "savings":
                 SavingsAccount.withdrawn_money(self)
                 self.authenticated = None
 
-    def __repr__(self) -> str:
-        return f"This is the return"        
-#         return f"Name is {self.client_info._name} Age is {self.client_info._age} \
-# Bank is {self.client_info.bank} Agency is {self.account_info.agency} Account is {self.account_info.number}"
-# # Type of Account is {self.type_account.account.type} \
+    def __repr__(self) -> str: 
+        os.system("cls")
+        return f'Your bank information is as follows: \n \
+        Your bank is {self.account_info.bank} bank.\n \
+        Your agency number is {self.account_info.agency}.\n \
+        Your account number is {self.account_info.number}.\n \
+        Your account is a {self.account_info.type}\n \
+        You currently have ${self.money_in_bank} in your account'
 
 ###################
+
+print("Welcome to the SimpleBank App! Here you can create your bank account, deposit and withdrawn money \
+easily. Let us begin!")
+print("First, we will need to collect some informations regarding yourself:")
 user = Bank()
 Bank.client_information(user)
-Bank.deposit_money_bank(user)
-Bank.withdrawn_money_bank(user)
-print(user)
+print()
 
-
-###################
-# client_one = None
-# client_one = Bank(client_one)
-# client_one = Bank.Create_Client(client_one)
-# client_one = Bank.Open_Bank_Account(client_one)
-
-# username = input("What is your name?\n")
-# user_age = input("What is your age?\n")
-# person1 = Client(username, user_age)
-# # print(person1)
-# person1.open_account()
-# # print()
-# # print()
-# # print(person1)
-# # print()
-# # print()
-# # print(person1.account)
-# # print()
-# # print()
-# person1.account.deposit_money()
-# person1.account.withdrawn_money()
-# person1.account.withdrawn_money()
-
-"""
-Arrumar 132 e 144 - Estão em comentário
-"""
+while True:
+    user_input = input("What would you like to do with your account?\n\
+        [D]eposit\n\
+        [W]ithdrawn\n\
+        [L]ist informations\n\
+        [S]ave your informations\n\
+        [E]xit the app\n").lower()
+    match user_input:
+        case "d":
+            os.system("cls")
+            user.deposit_money_bank()
+        case "w":
+            os.system("cls")
+            user.withdrawn_money_bank()
+        case "l":
+            print(user)
+            print()
+        case "s":
+            print("This option is not available in this version.")
+        case "e":
+            os.system("cls")
+            print("Thank you for using our service!")
+            break
+        case _:
+            print("You did not choose a valid option. Try again.")
